6462b1d0b2bd921c74b888fa18d8986c9839d400
KAngelika <angelika.kanata@gmail.com>
2022-07-07 07:14:24 +0100

    Initial commit

---
+++ vendingMachineModule/src/main/java/service/ServiceLayer.java
@@ -0,0 +1,18 @@
+package service;
+
+import dao.PersistenceException;
+import dto.Item;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+public interface ServiceLayer {
+
+
+    int decrementItemInventoryQty(String itemId) throws NoItemInventoryException, PersistenceException;
+
+    BigDecimal calculateReturnedChange(BigDecimal userMoneyBalance, Item item) throws InsufficientFundsException;
+
+    BigDecimal changeDueInPennies(BigDecimal itemCost, BigDecimal money);
+    Map<BigDecimal, BigDecimal> changeDuePerCoin(BigDecimal itemCost, BigDecimal money);
+}
